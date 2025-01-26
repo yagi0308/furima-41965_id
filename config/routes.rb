4ee5188067e565a9beef75789293979f1cf38050
yagi0308 <choodee@icloud.com>
2025-01-26 15:09:36 +0900

    ベーシック認証の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'items/index'
+  root to: "items#index"
   resources :items, only: [:index]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
